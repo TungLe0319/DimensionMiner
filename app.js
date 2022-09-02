fd83db1c666cb2048f667210ff259e01d1a02f49
Tung Le <tung.le0319@gmail.com>
2022-09-02 14:50:54 -0600

    upgrade into one

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let clickUpgrades = [
   { name: 'pickaxe', price: 5, quantity: 0, multiplier: 1 },
   { name: 'megapickaxe', price: 100, quantity: 0, multiplier: 5 },
+  { name: 'ultrapickaxe', price: 10, quantity: 0, multiplier: 20 },
 ];
 
 let automaticUpgrades = [
@@ -14,6 +15,7 @@ let automaticUpgrades = [
 let orbs = 0;
 let pickaxeCount = 0;
 let megaPickaxeCount = 0
+let ultraPickaxeCount = 0
 let laserCount = 0
 let megaLaserCount = 0
 let clickCountBonus = 0
@@ -21,6 +23,7 @@ let laserCountBonus = 0
 // ----------------
 let orbDOM = document.getElementById('orb-amount');
 let megaPickaxeCountDOM= document.getElementById('megapickaxe-count')
+let ultraPickaxeCountDOM= document.getElementById('ultraPickaxe-count')
 let pickaxeCountDOM = document.getElementById('pickaxe-count');
 let laserCountDOM = document.getElementById('laser-count')
 let MegaLaserCountDOM = document.getElementById('megalaser-count')
@@ -53,17 +56,22 @@ function mine() {
   upgrades.forEach(upgrade =>{
    orbs += upgrade.multiplier
   })
-
+  addLaser()
+ addAlien()
+colorchange()
   console.log(orbs);
   drawCounts();
 }
 
+
 function drawCounts() {
+  clickCountBonusDOM.innerText=clickCountBonus
   orbDOM.innerText = orbs;
   pickaxeCountDOM.innerText = pickaxeCount
   megaPickaxeCountDOM.innerText = megaPickaxeCount
   laserCountDOM.innerText = laserCount
   megaPickaxeCountDOM.innerText = megaLaserCount
+  ultraPickaxeCountDOM.innerText = ultraPickaxeCount
 }
 
 // SECTION CLICK UPGRADES
@@ -72,6 +80,7 @@ function upgradeOne() {
     orbs -= 5;
     upgrades.push(clickUpgrades[0]);
     pickaxeCount++;
+    clickCountBonus++
   }
   console.log(upgrades);
   drawCounts();
@@ -79,20 +88,40 @@ function upgradeOne() {
 }
 function upgradeTwo() {
   if (orbs >= 100) {
+   
     orbs -= 100;
     upgrades.push(clickUpgrades[1]);
     megaPickaxeCount++;
+    clickCountBonus+= 5
   }
   console.log(upgrades);
   drawCounts();
   
 }
+// change back to 500 later, using 10 for testing
+// change -= back to 500
+function upgradeThree() {
+  if (orbs >= 10) {
+    
+    orbs -= 10;
+    upgrades.push(clickUpgrades[2]);
+    ultraPickaxeCount++;
+    clickCountBonus+=20
+  }
+  console.log(upgrades);
+  drawCounts();
+  
+}
+
+
+
 // NOTE AUTO UPGRADES
 function autoUpgradeOne(){
   if (orbs >= 5) {
     orbs -= 5
     autoUpgrades.push(automaticUpgrades[0])
     laserCount ++
+    clickCountBonus+= 20
   }
   console.log(autoUpgrades);
   drawCounts()
@@ -104,12 +133,62 @@ function collectAutoUpgrades(){
     orbs += laser.multiplier
    
   })
-  console.log(autoUpgrades, orbs);
-  drawCounts()
+  // console.log(autoUpgrades, orbs);
+ drawCounts()
 }
 
 
 setInterval( collectAutoUpgrades, 3000)
+
+
+
+
+// SECTION TESTS
+
+function click(event) {
+  const template = document.getElementByID('#floating-text-template').content.cloneNode(true);
+  const element = template.querySelector('cookie') //replace class with yours
+  element.style.left = `${event.clientX}px`
+  element.style.top = `${event.clientY}px`
+  document.appendChild(element);
+}
+
+function colorchange(){
+  let orbElem =document.getElementById('atomOrb')
+  let rNum = Math.floor(Math.random() *360)
+orbElem.style.filter ='hue-rotate('+rNum +'deg)'
+console.log(rNum);
+}
+
+function addAlien(){
+  let alienMarquee = document.getElementById("alienmarquee")
+ 
+   let template = `<img src="./resources/Alien.png" alt="" srcset="" style="width: 2rem;">`
+  
+  alienMarquee.innerHTML += template
+}
+function addLaser(){
+  let laserMarquee = document.getElementById('lasermarquee')
+  let template = ` <img src="https://media2.giphy.com/media/1rOy1l3mulySaqilto/giphy.gif?cid=ecf05e47oc9d0htfhowppmrlwdqgkhipbq2wuokomsbcvi6f&rid=giphy.gif&ct=s" alt="" srcset="" style="width: 2rem;">`
+laserMarquee.innerHTML += template
+}
+
+
+
+// var x = 0;
+// $("#cookie").click(function(e) {
+//   x++;
+//   $("#cookie").append('<div id="x' + x + '" hidden>+1.0</div>');
+//   $("#x" + x).css("top", e.clientY);
+//   $("#x" + x).css("left", e.clientX - 10);
+//   $("#x" + x).css("position", "absolute");
+//   $("#x" + x).css("width", "25px");
+//   $("#x" + x).css("height", "25px");
+//   $("#x" + x).css("color", "white");
+//   $("#x" + x).css("font-weight", "bold");
+//   $("#x" + x).css("animation", "GoUp 2s forwards linear");
+//   $("#x" + x).show();
+// });
 
 
 function clickEffect(e) {
